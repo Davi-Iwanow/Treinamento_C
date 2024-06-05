b61f8dabfc21c26de111d5b17e02f9bb3d1c2c0c
Davi Iwanow <davi-iwanow123@hotmail.com>
2024-06-04 21:40:24 -0300

    variaveis e printf

---
+++ Exercicios/variaveis.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+    int a, i, j, k;
+    j=10;
+    i=5;
+    k=2;
+    a= 1;
+//tipos de variaveis 
+//float e double, tipo decimal e double tem o dobro de bytes
+// int - inteiros 
+// char - caractere
+// const - variavel só pode ter aquele valor
+
+//operações: % - resto da divisão 
+
+// a = a + 1 é a++
+    a++;
+    printf("o valor de a=%d\n",a);
+    printf("Helo world!\n");
+
+// Para imprimir variaveis:
+// %d- int
+// %f- float
+// %c- char
+// %.2f - 2 casas decimais 
+
+return 0;
+}
