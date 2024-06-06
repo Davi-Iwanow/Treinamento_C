de84cbd39aebab7f227288cd75f40a959c3a5e25
Davi Iwanow <davi-iwanow123@hotmail.com>
2024-06-05 22:48:00 -0300

    condicionamento e leituras

---
+++ leitura.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+//leitura da entrada
+
+int main(){
+    int x,y;
+    scanf("%d %d",&x,&y);
+    printf("Os valores digitados foram:%d e %d\n",x,y);
+
+//expressões relacionais 
+// igual ==  (1 é true, 0 é falso)
+// diferente !=
+// > e <
+// >= e <=
+
+//condicionais
+
+
+return 0;
+}
---
+++ paridade.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+    int x;
+
+    scanf("%d",&x);
+
+    if (x%2==0)
+    {
+        printf("seu numero e par");
+    }
+    else{
+        printf("seu numero e impar");
+    }
+
+
+
+
+
+return 0;
+}
