4446ddc3e2753dc084f57cb64e66620346d2b106
Davi Iwanow <davi-iwanow123@hotmail.com>
2024-07-04 11:57:37 -0300

    um pouco mais sobre structs

--- Exercicios/funções/estruturas.c
+++ Exercicios/funções/estruturas.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+
+#define TAM 4
 
 typedef struct pacientes{
     char nome[80];
@@ -7,30 +11,41 @@ typedef struct pacientes{
 } t_pacientes; 
 
 
-
 void imprime(t_pacientes a);
 
 
 int main(){
-    t_pacientes P;
+    int i;
+    t_pacientes P[TAM];
+    for(i=0;i<TAM;i++){
+        printf("Digite seu nome:\n");
+        scanf("%80[^\n]s",&P[i].nome);
+        fflush(stdin);
+        //ou usar o fgets
+        //fgets(P[i].nome,80,stdin);
 
-    printf("Digite seu nome:\n");
-    fgets(P.nome,80,stdin);
 
-    printf("Digite o dia:\n");
-    scanf("%d",&P.dia);
-    
-    printf("mes:\n");
-    scanf("%d",&P.mes);
 
-    printf("Ano:\n");
-    scanf("%d",&P.ano);
+        printf("Digite o dia:\n");
+        scanf("%d",&P[i].dia);
+        fflush(stdin);
 
-    imprime(P);// Passar a variável P, não o tipo t_pacientes
+        printf("mes:\n");
+        scanf("%d",&P[i].mes);
+        fflush(stdin);
+
+        printf("Ano:\n");
+        scanf("%d",&P[i].ano);
+        fflush(stdin);
+
+    }
+    imprime(P[0]);// Passar a variável P, não o tipo t_pacientes
     
 return 0;
 }
 void imprime(t_pacientes a){
+
+    system("cls");
     
     printf("Seu nome: %s\nVoce nasceu em %d/%d/%d\n",a.nome,a.dia,a.mes,a.ano);
     
